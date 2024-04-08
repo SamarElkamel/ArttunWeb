dd58efc4de44ffdac57918dd0ffab307a8559dfe
FanTasy CriT <fantasycrit20@gmail.com>
2024-04-08 18:11:51 +0200

    afficher_user

---
+++ src/Entity/Adresses.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Adresses
+ *
+ * @ORM\Table(name="adresses")
+ * @ORM\Entity
+ */
+class Adresses
+{
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="country", type="string", length=50, nullable=false)
+     */
+    private $country;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="state", type="string", length=50, nullable=true)
+     */
+    private $state;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="city", type="string", length=50, nullable=false)
+     */
+    private $city;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="code", type="integer", nullable=false)
+     */
+    private $code;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="street", type="string", length=50, nullable=false)
+     */
+    private $street;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="lon", type="float", precision=10, scale=0, nullable=false)
+     */
+    private $lon;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="lat", type="float", precision=10, scale=0, nullable=false)
+     */
+    private $lat;
+
+    /**
+     * @var \User
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="NONE")
+     * @ORM\OneToOne(targetEntity="User")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="id", referencedColumnName="adresse")
+     * })
+     */
+    private $id;
+
+
+}
