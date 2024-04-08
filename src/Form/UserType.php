f76f3123498ad7ad83de237d8643994df0e8ecd9
FanTasy CriT <fantasycrit20@gmail.com>
2024-04-08 23:22:46 +0200

    user_count added form for editing user

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -12,13 +12,45 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('prenom')
-            ->add('type')
-            ->add('photo')
-            ->add('adresse')
-            ->add('adresseMail')
-            ->add('mdp');
+            ->add('nom', null, [
+                'attr' => ['placeholder' => 'Placeholder'],
+                'label' => 'First Name',
+                'row_attr' => ['class' => 'row'],
+                'label_attr' => ['class' => 'active'],
+                'attr' => ['id' => 'first_name']
+            ])
+            ->add('prenom', null, [
+                'label' => 'Last Name',
+                'row_attr' => ['class' => 'row'],
+                'label_attr' => ['class' => 'active'],
+                'attr' => ['id' => 'last_name']
+            ])
+            ->add('type', null, [
+                'disabled' => true,
+                'label' => 'Disabled',
+                'row_attr' => ['class' => 'row'],
+                'label_attr' => ['class' => 'active'],
+                'attr' => ['id' => 'disabled']
+            ])
+            ->add('mdp', null, [
+                'label' => 'Password',
+                'row_attr' => ['class' => 'row'],
+                'label_attr' => ['class' => 'active'],
+                'attr' => ['id' => 'password']
+            ])
+            ->add('adresseMail', null, [
+                'label' => 'Email',
+                'row_attr' => ['class' => 'row'],
+                'label_attr' => ['class' => 'active'],
+                'attr' => ['id' => 'email']
+            ])
+            ->add('photo', null, [
+                'label' => 'Photo',
+                'row_attr' => ['class' => 'row'],
+                'label_attr' => ['class' => 'active'],
+                'attr' => ['id' => 'photo']
+            ]);
+
     }
 
     public function configureOptions(OptionsResolver $resolver): void
