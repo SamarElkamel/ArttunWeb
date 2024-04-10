55c206da0b4ab6bb7d54d8570a0ab0699bd38c26
FanTasy CriT <fantasycrit20@gmail.com>
2024-04-11 00:34:12 +0200

    edit and delete functionnal

--- src/Controller/UserCrudController.php
+++ src/Controller/UserCrudController.php
@@ -26,4 +26,46 @@ class UserCrudController extends AbstractController
             'controller_name' => 'UserCrudController',
         ]);
     }
+    #[Route('/back/user/crud/edit', name: 'app_user_crud_edit')]
+    public function edit(EntityManagerInterface $entityManager, Request $request){
+        echo "ID: " . $request->request->get('passwordfield') . "<br>";
+        $id = $request->request->get('idfield');
+        $nom = $request->request->get('nomfield');
+        $prenom = $request->request->get('prenomfield');
+        $mail = $request->request->get('mailfield');
+        $password = $request->request->get('passwordfield');
+        $user = $entityManager->getRepository(User::class)->find($id);
+
+        if (!$user) {
+            throw $this->createNotFoundException('User not found');
+        }
+
+        // Update user properties
+        $user->setNom($nom);
+        $user->setPrenom($prenom);
+        $user->setAdresseMail($mail);
+        $user->setMdp($password);
+        // Set other properties...
+
+        // Persist changes to the database
+        $entityManager->flush();
+
+        // Optionally, redirect to a success page or return a response
+        return $this->redirectToRoute('app_user_crud');
+    }
+    #[Route('/back/user/crud/delete{id}', name: 'app_user_crud_delete')]
+    public function delete(EntityManagerInterface $entityManager, Request $request,$id){
+        $userRepository = $entityManager->getRepository(User::class);
+        $user = $userRepository->find($id);
+        if (!$user) {
+            throw $this->createNotFoundException('User not found');
+        }
+        try {
+            $entityManager->remove($user);
+            $entityManager->flush();
+        } catch (\Exception $e) {
+            return new Response('Error: ' . $e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+        return $this->redirectToRoute('app_user_crud');
+    }
 }
