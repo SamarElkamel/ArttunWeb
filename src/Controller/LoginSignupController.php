cf2f1973e54bbea255feb7544eff0d02690b6924
samar <samar_elkamel@outlook.fr>
2024-04-23 19:03:48 +0100

    Merge branch 'main' of https://github.com/SamarElkamel/SymfonyPIDEV

--- src/Controller/LoginSignupController.php
+++ src/Controller/LoginSignupController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\user\User;
+use App\Form\UserType1;
 use App\Repository\user\UserRepository;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Exception\ORMException;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,8 +23,9 @@ class LoginSignupController extends AbstractController
     {
         $this->session = $session;
     }
-    public function index(EntityManagerInterface $entityManager, AuthenticationUtils $authenticationUtils,UserRepository $userrepo,Request $request): Response
+    /*public function index(EntityManagerInterface $entityManager, AuthenticationUtils $authenticationUtils,UserRepository $userrepo,Request $request)
     {
+
         if ($request->isMethod('POST')) {
             $email = $request->request->get('email');
             $password = $request->request->get('password');
@@ -35,5 +40,20 @@ class LoginSignupController extends AbstractController
                 return $this->redirectToRoute('app_login_signup');
             }
         }
+    }*/
+#[Route('/signup', name: 'app_signup')]
+public function signup(ManagerRegistry $repository ,Request $request):Response{
+    $user = new User();
+    $em=$repository->getManager();
+    $form = $this->createForm(UserType1::class,$user);
+    $form->handleRequest($request);
+    if($form->isSubmitted() && $form->isValid() ){
+            $user->setAdresse(52);
+        $user->setPhoto("52");
+            $em->persist($user);
+            $em->flush();
+            return $this->redirectToRoute("app_user_crud");
     }
+    return $this->render('login_signup/signup.html.twig',['form'=>$form->createView()]);
+}
 }
