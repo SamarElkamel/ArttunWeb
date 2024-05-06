11f2e99060b546a3479bee9f0e5272f46b723363
FanTasy CriT <fantasycrit20@gmail.com>
2024-05-06 14:56:24 -0700

    livreur

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Scheb\TwoFactorBundle\SchebTwoFactorBundle::class => ['all' => true],
     Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
